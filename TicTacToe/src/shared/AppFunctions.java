d7edba0e1778affdf42b3bdac2394fd11d2e7525
Youssif Nasser <youssifn.mostafa@gmail.com>
2025-01-05 01:16:28 +0200

    handled pop up

--- TicTacToe/src/shared/AppFunctions.java
+++ TicTacToe/src/shared/AppFunctions.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import tictactoe.TicTacToe;
 import tictactoe.setting.FXMLSettingController;
@@ -15,11 +16,19 @@ import tictactoe.setting.FXMLSettingController;
  */
 public abstract class AppFunctions {
 
-    public static void goTo(ActionEvent actionEvent, Parent root){
+    public static void goTo(ActionEvent actionEvent, Parent root) {
         Scene scene = new Scene(root);
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void pop(Stage ownerStage, Parent root) {
+        Stage newStage = new Stage();
+        newStage.setScene(new Scene(root));
+        newStage.initOwner(ownerStage);
+        newStage.initModality(Modality.WINDOW_MODAL);
+        newStage.show();
     }
 
 }
