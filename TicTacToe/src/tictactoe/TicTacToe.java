621045252e40aa6ea83506109f9a1cabf78c8e49
Youssif Nasser <youssifn.mostafa@gmail.com>
2025-01-06 17:26:55 +0200

    merged model classes

--- TicTacToe/src/tictactoe/TicTacToe.java
+++ TicTacToe/src/tictactoe/TicTacToe.java
@@ -6,21 +6,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import shared.AppString;
 
-
 import tictactoe.splashscreengui.FXMLSplashScreenController;
 
 public class TicTacToe extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        //   Parent root = new FXMLSettingController();
-        //Parent root = new FXMLSignupController();
-
 
         Parent root = new FXMLSplashScreenController(stage);
-
-
-
         Scene scene = new Scene(root);
         stage.setTitle(AppString.APP_TITLE);
         stage.setScene(scene);
