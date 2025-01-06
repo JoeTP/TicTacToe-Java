bde6d8735f679265f6640f9b8eb8568dafee1827
Youssif Nasser <youssifn.mostafa@gmail.com>
2025-01-06 16:48:47 +0200

    class improved

--- TicTacToe/src/shared/AppFunctions.java
+++ TicTacToe/src/shared/AppFunctions.java
@@ -1,15 +1,11 @@
 package shared;
 
-import java.io.IOException;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import tictactoe.TicTacToe;
-import tictactoe.setting.FXMLSettingController;
 
 /**
  * Here we handle commonly used functions like navigating to another scene.
