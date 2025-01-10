1f0cbdf9e239ca60eafdd7bf5d0dfbe1760915ee
Mario <90201555+MarioAbdelMaseeh@users.noreply.github.com>
2025-01-10 02:11:15 +0200

    modofications

--- TicTacToe/src/tictactoe/signup/FXMLSignupController.java
+++ TicTacToe/src/tictactoe/signup/FXMLSignupController.java
@@ -16,6 +16,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import models.UserModel;
@@ -50,24 +51,26 @@ public class FXMLSignupController extends FXMLSignupBase {
 
     @Override
     protected void goToActiveUsers(ActionEvent actionEvent) {
-        UserModel user = new UserModel();
-        user = getNewUserData();
-        Client client = new Client();
-        client.connectToServer();
-        try {
-            client.sendUser(user);
-        } catch (IOException ex) {
-            Logger.getLogger(FXMLSignupController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println(client.serverStatus);
-        if(client.serverStatus == false){
-            System.out.println(user.getName()+"\n"+user.getEmail()+"\n"+user.getPassword()+"\n"+user.getImage());
-            Platform.runLater(()->{
+        UserModel user = getNewUserData();
+        if (user != null){
+            Client client = new Client();
+            client.connectToServer();
+            try {
+                client.sendUser(user);
+            } catch (IOException ex) {
+                Logger.getLogger(FXMLSignupController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println(client.serverStatus);
+            if(client.serverStatus == false){
+                System.out.println(user.getName()+"\n"+user.getEmail()+"\n"+user.getPassword()+"\n"+user.getImage());
+                Platform.runLater(()->{
                 AppFunctions.closePopup(actionEvent);
                 AppFunctions.goTo(actionEvent, new FXMLPlayerVsPlayerOnlineController(stage));
             });
             
         }
+        }
+        
         
     }
 
@@ -85,10 +88,37 @@ public class FXMLSignupController extends FXMLSignupBase {
     }
     protected UserModel getNewUserData(){
         UserModel user = new UserModel();
-        user.setName(usernameTextField.getText());
-        user.setEmail(emailTextField.getText());
+        boolean valid = true;
+        String regexPattern = "[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,}$";
+        if(usernameTextField.getText().length() > 6)
+        {
+            user.setName(usernameTextField.getText());
+        }else{
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Username is smaller than 6 letters.");
+            alert.showAndWait();
+            valid = false;
+        }
+        if(emailTextField.getText().matches(regexPattern)){
+            user.setEmail(emailTextField.getText());
+        }else{
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Invalid email address");
+            alert.showAndWait();
+            valid = false;
+        }
+        if(passwordField.getText().length() > 6){
+            user.setPassword(passwordField.getText());
+        }else{
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Password is smaller than 6 letters.");
+            alert.showAndWait();
+            valid = false;
+        }
         user.setImage(Integer.toString(currentImageIndex));
-        user.setPassword(passwordField.getText());
-        return user;
+        if(valid){
+            return user;
+        }else{
+            return null;
+        }
+        
     }
 }
+ 
