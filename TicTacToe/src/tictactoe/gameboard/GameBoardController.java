d736645baa69f9a464df350fb9fa92a8bbed7e75
Ayat Gamal <ayat.ga106@gmail.com>
2025-01-17 03:36:11 +0200

    make timer more synchronized and apply some style

--- TicTacToe/src/tictactoe/gameboard/GameBoardController.java
+++ TicTacToe/src/tictactoe/gameboard/GameBoardController.java
@@ -24,6 +24,7 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
+import javafx.scene.Node;
 import javafx.util.Duration;
 import models.ComputerPlayer;
 import models.Player;
@@ -113,9 +114,11 @@ public class GameBoardController extends FXMLGameBoardBase {
             System.out.println("current Move: " + move);
 
             if (playerOne.hisTurn) {
-                grid.setDisable(false);//enable for palyer
+                startCountdownTimer();
+                enableButtons();//enable for palyer
                 b.setText(playerOne.getChar());
             } else {
+                startCountdownTimer();
                 b.setText(playerTwo.getChar());
             }
 
@@ -131,22 +134,25 @@ public class GameBoardController extends FXMLGameBoardBase {
             checkPlayerWinner();
 
             if (!isEndOfGame) {
-                if (playerTwo instanceof ComputerPlayer && playerTwo.hisTurn) { //disable for computer
-                    grid.setDisable(true);
-
+                if (playerTwo instanceof ComputerPlayer && playerTwo.hisTurn) { 
+                    disableButtons();//disable for computer
+                                    startCountdownTimer();
                     Timeline timeline = new Timeline(new KeyFrame(
-                            Duration.seconds(1),
+                            Duration.seconds(1.4),
                             event -> {
                                 makeComputerMove();
-                                grid.setDisable(false);
+                                Platform.runLater(() -> {
+                                    enableButtons();
+                                });
                             }
                     ));
                     timeline.setCycleCount(1);
                     timeline.play();
+                } else {
+                    startCountdownTimer();
                 }
             }
             printGame();
-            startCountdownTimer();
         }
     }
 
@@ -156,10 +162,10 @@ public class GameBoardController extends FXMLGameBoardBase {
         if (timeLine != null) {
             timeLine.stop();   //3l4an law startCountdownTimer 3mnlnlha call multiple time
         }
-        
+
         if (!isEndOfGame) {
             timeLine = new Timeline(
-                    new KeyFrame(Duration.seconds(1), (ActionEvent event) -> {
+                    new KeyFrame(Duration.seconds(0.8), (ActionEvent event) -> {
                         timer.setStyle("-fx-text-fill: #3E5879;");
                         timer.setText(" " + countdownTime + "");
 
@@ -271,9 +277,6 @@ public class GameBoardController extends FXMLGameBoardBase {
             } else {
                 System.out.println("insiidee makeAutomaticMove and the button not existt");
             }
-
-            return;
-
         } else {
             System.out.println("No empty cells found.");
         }
@@ -455,7 +458,7 @@ public class GameBoardController extends FXMLGameBoardBase {
         return (value % 2 == 0) ? O_CHAR : X_CHAR;
     }
 
-    void changingLabelsStyles() {
+    private void changingLabelsStyles() {
         Platform.runLater(() -> {
             if (playerOne.hisTurn) {
                 playerOneLabel.setTextFill(Paint.valueOf("#21bd5c")); //green
@@ -470,6 +473,22 @@ public class GameBoardController extends FXMLGameBoardBase {
                 playerOneChar.setTextFill(Paint.valueOf("#3e5879")); //green
             }
         });
+    }
+
+    private void disableButtons() {
+        for (Node n : grid.getChildren()) {
+            if (n instanceof Button) {
+                n.setDisable(true);
+            }
+        }
+    }
+
+    private void enableButtons() {
+        for (Node n : grid.getChildren()) {
+            if (n instanceof Button) {
+                n.setDisable(false);
+            }
+        }
     }
 
     @Override
