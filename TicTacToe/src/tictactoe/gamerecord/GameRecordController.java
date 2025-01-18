23539c1542c51fa1bd135cda2a0de36924f4d083
alyaa149 <lohussien2@gmail.com>
2025-01-18 11:34:18 +0200

    fix next and previous btn

--- TicTacToe/src/tictactoe/gamerecord/GameRecordController.java
+++ TicTacToe/src/tictactoe/gamerecord/GameRecordController.java
@@ -17,68 +17,33 @@ public class GameRecordController extends GameRecordBase {
     Stage stage;
     GameModel game = null;
     String fileName;
+        Integer[][] board;  
+     int currentStep = 1;
 
-    Integer[][] currentBoard = new Integer[3][3]; 
-    List<Integer[][]> boardStates = new ArrayList<>(); 
-    int currentMoveIndex = 0; 
+    
 
        public GameRecordController(Stage stage, String fileName) {
-        this.stage = stage;
+         this.stage = stage;
         this.fileName = fileName;
-        getDataFromFile();
+        loadGameData();
     }
 
-    @Override
-    protected void handleExitButton(ActionEvent actionEvent) {
-        AudioController.clickSound();
-        AppFunctions.closePopup(actionEvent);
-    }
-
-    protected void getDataFromFile() {
+    private void loadGameData() {
         game = LaodGame.loadGameFromFile(fileName);
-        generateBoardStates(game);
-        showInGameBoard(boardStates.get(currentMoveIndex));
+        board = game.getBoard();
+        showBoard(currentStep);
     }
 
-    private void generateBoardStates(GameModel game) {
-        Integer[][] board = game.getBoard();
-        boardStates.add(copyBoard(board));
-
-        for (int moveIndex = 1; moveIndex <= 9; moveIndex++) {
-            Integer[][] newBoard = copyBoard(board);
-            for (int row = 0; row < 3; row++) {
-                for (int col = 0; col < 3; col++) {
-                    if (newBoard[row][col] != null && newBoard[row][col] == moveIndex) {
-                        if (moveIndex % 2 == 1) {
-                            newBoard[row][col] = 1;
-                        } else {
-                            newBoard[row][col] = 2;
-                        }
-                    }
-                }
-            }
-            boardStates.add(newBoard);
-        }
-    }
-
-    private Integer[][] copyBoard(Integer[][] board) {
-        Integer[][] newBoard = new Integer[3][3];
-        for (int i = 0; i < 3; i++) {
-            System.arraycopy(board[i], 0, newBoard[i], 0, 3);
-        }
-        return newBoard;
-    }
-
-    public void showInGameBoard(Integer[][] board) {
+    private void showBoard(int step) {
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
                 Integer value = board[row][col];
                 Button button = getButtonsByRowAndColumn(col, row);
 
-                if (value != null) {
-                    if (value == 1) {
+                if (value != null && value <= step) {
+                    if (value % 2 == 1) {
                         button.setText("X");
-                    } else if (value == 2) {
+                    } else {
                         button.setText("O");
                     }
                 } else {
@@ -88,42 +53,64 @@ public class GameRecordController extends GameRecordBase {
         }
     }
 
-    private Button getButtonsByRowAndColumn(int c, int r) {
+   private Button getButtonsByRowAndColumn(int c, int r) {
         String key = c + "," + r;
-        switch (key) {
-            case "0,0": return b00;
-            case "0,1": return b01;
-            case "0,2": return b02;
-            case "1,0": return b10;
-            case "1,1": return b11;
-            case "1,2": return b12;
-            case "2,0": return b20;
-            case "2,1": return b21;
-            case "2,2": return b22;
-            default: return null;
+
+        if ("0,0".equals(key)) {
+            return b00;
         }
+        if ("0,1".equals(key)) {
+            return b01;
+        }
+        if ("0,2".equals(key)) {
+            return b02;
+        }
+        if ("1,0".equals(key)) {
+            return b10;
+        }
+        if ("1,1".equals(key)) {
+            return b11;
+        }
+        if ("1,2".equals(key)) {
+            return b12;
+        }
+        if ("2,0".equals(key)) {
+            return b20;
+        }
+        if ("2,1".equals(key)) {
+            return b21;
+        }
+        if ("2,2".equals(key)) {
+            return b22;
+        }
+
+        return null;
     }
+
 
     @Override
     protected void handlePreviousBtn(ActionEvent actionEvent) {
-              AudioController.clickSound();
-        if (currentMoveIndex > 0) {
-            currentMoveIndex--;
-            currentBoard = boardStates.get(currentMoveIndex);
-            showInGameBoard(currentBoard);
+        if (currentStep > 1) {
+            currentStep--;
+            showBoard(currentStep);
         }
     }
 
     @Override
     protected void handleNextBtn(ActionEvent actionEvent) {
-              AudioController.clickSound();
-        if (currentMoveIndex < boardStates.size() - 1) {
-            currentMoveIndex++;
-            currentBoard = boardStates.get(currentMoveIndex);
-            showInGameBoard(currentBoard);
+        if (currentStep < 9) {
+            currentStep++;
+            showBoard(currentStep);
         }
     }
+
+    @Override
+    protected void handleExitButton(ActionEvent actionEvent) {
+        AudioController.clickSound();
+        AppFunctions.closePopup(actionEvent);
     }
+    }
+    
 
     
    
