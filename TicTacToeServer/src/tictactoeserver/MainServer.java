8b63d6318bf81787fca0ece8ca4f50b62634c030
Youssif Nasser <youssifn.mostafa@gmail.com>
2025-01-18 03:09:21 +0200

    fixed server exit close threads

--- TicTacToeServer/src/tictactoeserver/MainServer.java
+++ TicTacToeServer/src/tictactoeserver/MainServer.java
@@ -1,15 +1,16 @@
-
 package tictactoeserver;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import tictactoeserver.gui.FXMLServerController;
 
 public class MainServer extends Application {
 
-    
     @Override
     public void start(Stage stage) {
         Parent root = new FXMLServerController();
@@ -18,6 +19,11 @@ public class MainServer extends Application {
         stage.setScene(scene);
         stage.show();
         stage.setResizable(false);
+
+        stage.setOnCloseRequest((WindowEvent e) -> {
+            Platform.exit();
+            System.exit(0);
+        });
     }
 
     public static void main(String[] args) {
